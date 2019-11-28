ec96965f583ce55c53449b47708cb48656f4d14d
Jaime Wu <jwu153@UoA.auckland.ac.nz>
2019-11-29 11:58:24 +1300

    added swagger ui

--- FitTracAPI/FitTracAPI/Startup.cs
+++ FitTracAPI/FitTracAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using FitTracAPI.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace FitTracAPI
 {
@@ -31,11 +32,39 @@ namespace FitTracAPI
 
             services.AddDbContext<FitTracAPIContext>(options =>
                     options.UseSqlite(Configuration.GetConnectionString("FitTracAPIContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "FitTracAPI",
+                    Version = "v1",
+                    Description = "A web API providing a custom toolkit for Managing Fitness Programs",
+                    Contact = new Contact
+                    {
+                        Name = "Jaime Wu",
+                        Email = "jaime.wu011@gmail.com",
+                        Url = "https://github.com/Zefty/FitTracAPI"
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ScribrAPI V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
