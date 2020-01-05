83f1add8999a2f39564a6320e1f4ba0ebc692786
Jaime Wu <jaime.wu011@gmail.com>
2020-01-05 23:28:31 +1300

    fix editworkout put method

--- FitTracAPI/Controllers/WorkoutsController.cs
+++ FitTracAPI/Controllers/WorkoutsController.cs
@@ -85,7 +85,7 @@ namespace FitTracAPI.Controllers
             return NoContent();
         }
 
-        [HttpPut("EditWorkout{id}")]
+        [HttpPut("EditWorkout/{id}")]
         public async Task<IActionResult> EditWorkout(int id, Workout workout)
         {
             if (id != workout.WorkoutId)
